d4615323e968442e1949179cd17cd924dfc1f779
javiervasquez212 <60985490+javiervasquez212@users.noreply.github.com>
2023-02-21 23:53:50 -0600

    Delete programa.cpp

--- programa.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-   cout << " este es el codigo que hizo Pique " << endl;
-      cout << " este es el codigo que hizo Pique " << endl;
-   cout << " ================================ " << endl;
-}
