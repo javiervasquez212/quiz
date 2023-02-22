7cc9a105e1f5d71b014d67d01c6d4812ebf402b2
Javier Vasquez <javiervasquez212@gmail.com>
2023-02-22 00:21:45 -0600

    shakira

---
+++ programa.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+   cout << " este es el codigo que hizo Shakira" << endl;
+   cout << " ================================ " << endl;
+
+}
